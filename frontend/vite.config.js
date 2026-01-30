f191640d523ddcc5ecb5fda56e4d22906dac5356
David Sliren <bluee.97.berry@gmail.com>
2026-01-30 18:25:19 -0500

    fix(vit/base): corregir error de la pagina en blanco, depues de hacer el deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/futbol-mania/",
+  base: "/Futbol-Mania/",
   server: {
     proxy: {
       "/api": "http://localhost:3000",
