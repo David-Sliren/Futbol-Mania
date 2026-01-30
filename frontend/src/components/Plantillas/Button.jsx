1ff348290d81a315809e1d2c81218b145ce83e08
David Sliren <bluee.97.berry@gmail.com>
2026-01-30 12:23:59 -0500

    style(frontend/Button) Diminuir tamaño del componente Button

--- frontend/src/components/Plantillas/Button.jsx
+++ frontend/src/components/Plantillas/Button.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Button() {
   return (
     <button
-      className="bg-white text-center w-48 rounded-2xl h-14 relative text-black text-xl font-semibold group cursor-pointer"
+      className="bg-white/20 backdrop-blur-xl text-center w-38 rounded-2xl h-14 relative text-white text-xl font-semibold group cursor-pointer"
       type="button"
     >
-      <div className="bg-green-400 rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+      <div className="bg-green-500 rounded-xl h-12 w-[30%] flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[95%] z-10 duration-500">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1024 1024"
